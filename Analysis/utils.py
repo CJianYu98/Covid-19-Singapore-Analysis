32ad42950a33bd60f146733b7fb09fcdbef47977
owlingjia <lingjia.ow.2019@sis.smu.edu.sg>
2021-03-21 22:09:30 +0800

    update utils.py

--- Analysis/utils.py
+++ Analysis/utils.py
@@ -75,3 +75,73 @@ def get_valuable_comments(df, label = 1.0):
     valuable_comments = df[df['valuable'] == label]
 
     return valuable_comments
+
+def get_merged_policy_df(policy):
+    df_list = []
+    path_parent = "/Users/joshuawong/Documents/GitHub" # Change main path accordingly
+
+    # Reddit
+    reddit_folder = f"{path_parent}/Covid-19-Singapore-Analysis/Data/Reddit Data/Cleaned Data/Policies/Combined"
+    reddit_files = [file for file in os.listdir(reddit_folder) if file != '.DS_Store']
+    for file in reddit_files:
+        if policy in file.lower():
+            reddit_policy_file = file
+            reddit_df = pd.read_csv(f'{reddit_folder}/{reddit_policy_file}')
+            reddit_df = reddit_df[['comment_body', 'comment_created_dts']]
+            reddit_df.rename(columns={"comment_body": 'text', 'comment_created_dts':'datetime'}, inplace=True)
+            df_list.append(reddit_df)
+            break 
+
+    # Twitter 
+    twitter_folder = f"{path_parent}/Covid-19-Singapore-Analysis/Data/Twitter Data/Cleaned Data/Policies/Combined"
+    twitter_files = [file for file in os.listdir(twitter_folder) if file != '.DS_Store']
+    for file in twitter_files:
+        if policy in file.lower():
+            twitter_policy_file = file
+            twitter_df = pd.read_csv(f'{twitter_folder}/{twitter_policy_file}')
+            twitter_df["datetime"] = twitter_df["date"] + " " + twitter_df["time"]
+            twitter_df = twitter_df[['tweet', 'datetime']]
+            twitter_df.rename(columns={"tweet": 'text'}, inplace=True)
+            df_list.append(twitter_df)
+            break
+
+    # Instagram
+    insta_folder = f"{path_parent}/Covid-19-Singapore-Analysis/Data/Instagram Data/Cleaned Data/Policies/Combined"
+    insta_files = [file for file in os.listdir(insta_folder) if file != '.DS_Store']
+    for file in insta_files:
+        if policy in file:
+            insta_policy_file = file
+            insta_df = pd.read_csv(f'{insta_folder}/{insta_policy_file}')
+            insta_df = insta_df[['Comments', 'Comment Datetime']]
+            insta_df.rename(columns = {"Comments": 'text', 'Comment Datetime':'datetime'}, inplace=True)
+            df_list.append(insta_df)
+            break
+
+
+
+    # Hardwarezone 
+    hwz_folder = f"{path_parent}/Covid-19-Singapore-Analysis/Data/Hardwarezone Data/Cleaned Data"
+    hwz_files = [file for file in os.listdir(hwz_folder) if file != '.DS_Store']
+    for file in hwz_files:
+        if policy in file:
+            hwz_policy_file = file
+            hwz_df = pd.read_csv(f'{hwz_folder}/{hwz_policy_file}')
+            hwz_df = hwz_df[['Comments', 'Thread Datetime']]
+            hwz_df.rename(columns = {"Comments": 'text', 'Thread Datetime':'datetime'}, inplace=True)
+            df_list.append(hwz_df)
+            break
+
+    # # Facebook 
+    # facebook_folder = f"{path_parent}/Covid-19-Singapore-Analysis/Data/Hardwarezone Data/Cleaned Data" # To change
+    # facebook_files = [file for file in os.listdir(facebook_folder) if file != '.DS_Store']
+    # for file in facebook_files:
+    #     if policy in file:
+    #         facebook_policy_file = file
+    #         facebook_df = pd.read_csv(f'{facebook_folder}/{facebook_policy_file}')
+    #         facebook_df = facebook_df[['Comments', 'Comment Datetime']]
+    #         facebook_df.rename(columns = {"Comments": 'text', 'Comment Datetime':'datetime'}, inplace=True)
+    #         df_list.append(facebook_df)
+    #         break
+            
+    final_df = pd.concat(df_list)
+    return final_df
